9f4cbf00cc1b11400bb63ae84fecdce7258eafb9
이진우 <leejinwoo1126@gmail.com>
2025-07-23 21:29:32 +0900

    refactor: AppConfig extract method

--- src/main/java/com/example/core/config/AppConfig.java
+++ src/main/java/com/example/core/config/AppConfig.java
@@ -1,6 +1,9 @@
 package com.example.core.config;
 
+import com.example.core.discount.DiscountPolicy;
 import com.example.core.discount.FixDiscountPolicy;
+import com.example.core.discount.RateDiscountPolicy;
+import com.example.core.repository.MemberRepository;
 import com.example.core.repository.MemoryMemberRepository;
 import com.example.core.service.MemberService;
 import com.example.core.service.MemberServiceImpl;
@@ -9,10 +12,18 @@ import com.example.core.service.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), getDiscountPolicy());
+    }
+
+    public DiscountPolicy getDiscountPolicy() {
+        return new RateDiscountPolicy();
     }
 }
