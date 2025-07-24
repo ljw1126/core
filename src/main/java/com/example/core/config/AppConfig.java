7e16ea408d989c3de7bcac414897e61364d5c658
이진우 <leejinwoo1126@gmail.com>
2025-07-24 09:16:50 +0900

    refactor: add spring configuration bean annotation

--- src/main/java/com/example/core/config/AppConfig.java
+++ src/main/java/com/example/core/config/AppConfig.java
@@ -1,7 +1,6 @@
 package com.example.core.config;
 
 import com.example.core.discount.DiscountPolicy;
-import com.example.core.discount.FixDiscountPolicy;
 import com.example.core.discount.RateDiscountPolicy;
 import com.example.core.repository.MemberRepository;
 import com.example.core.repository.MemoryMemberRepository;
@@ -9,20 +8,28 @@ import com.example.core.service.MemberService;
 import com.example.core.service.MemberServiceImpl;
 import com.example.core.service.OrderService;
 import com.example.core.service.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), getDiscountPolicy());
     }
 
+    @Bean
     public DiscountPolicy getDiscountPolicy() {
         return new RateDiscountPolicy();
     }
